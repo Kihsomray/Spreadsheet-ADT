848e7ac59cb54c121b86ebb3cc4b7c0c81d99799
Matt <73551894+osDreamscape@users.noreply.github.com>
2023-02-27 22:21:15 -0800

    Fleshed out the Element classes and Java documentation.

--- src/model/SpreadSheet.java
+++ src/model/SpreadSheet.java
@@ -4,4 +4,13 @@ public class SpreadSheet {
 
     private Cell[][] sheet;
 
+    /**
+     * Getter for a Cell object in the Spreadsheet.
+     * @param theColumn The column we want.
+     * @param theRow The row we want.
+     * @return The cell at (theColumn, theRow).
+     */
+    public Cell getCell(int theColumn, int theRow) {
+        return sheet[theColumn][theRow];
+    }
 }
--- src/model/element/value/CellElement.java
+++ src/model/element/value/CellElement.java
@@ -2,10 +2,29 @@ package model.element.value;
 
 public class CellElement implements ValueElement {
 
+    /**
+     * The integer representation of the CellElement.
+     */
+    private int myCellValue;
+
+    /**
+     * Constructor for the CellElement.
+     * @param theValue The value
+     */
+    public CellElement(final int theValue) {
+        setValue(theValue);
+    }
 
     @Override
     public int getValue() {
-        return 0;
+        return myCellValue;
     }
 
+    /**
+     * Private setter for the integer value, primarily used by constructor.
+     * @param theValue the integer we are setting the CellValue to represent.
+     */
+    private void setValue(final int theValue) {
+        myCellValue = theValue;
+    }
 }
