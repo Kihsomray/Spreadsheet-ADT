60b0fe6a3a45466fa1b3c7d4c5324f17579c466e
JohnCongroo <113878822+JohnCongroo@users.noreply.github.com>
2023-03-08 11:53:39 -0800

    gui load, values do not update unless cell is clicked on

--- src/view/menu/FileMenu.java
+++ src/view/menu/FileMenu.java
@@ -1,5 +1,7 @@
 package view.menu;
 
+import controller.SpreadSheetBuilder;
+import model.SpreadSheet;
 import view.SSFrame;
 
 import javax.swing.*;
@@ -17,7 +19,7 @@ public class FileMenu extends JMenu {
     /**
      * JFrame containing spreadsheet application.
      */
-    private final SSFrame myFrame;
+    private SSFrame myFrame;
 
     /**
      * MenuItem to open a file in the spreadsheet.
@@ -77,10 +79,26 @@ public class FileMenu extends JMenu {
         openFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, KeyEvent.CTRL_DOWN_MASK));
         openFile.addActionListener(theEvent -> {
             JFileChooser fileChooser = new JFileChooser();
-            int result = fileChooser.showSaveDialog(null);
+            int result = fileChooser.showOpenDialog(null);
             if (result == JFileChooser.APPROVE_OPTION) {
                 String file = String.valueOf((fileChooser.getSelectedFile()));
-                myFrame.getMySpreadSheet().loadSheet(file);
+
+                String[] fileData = myFrame.getMySpreadSheet().loadSheet(file); //read file
+
+                int rowSize = Integer.parseInt(fileData[0]);
+                int colSize = Integer.parseInt(fileData[1]); //retrieve col and row size
+
+                //initialize new spreadsheet
+                SpreadSheet ss = new SpreadSheet(new SpreadSheetBuilder(colSize,rowSize),colSize,rowSize);
+
+                for (int i = 2; i < fileData.length; i++) {
+                    String[] elements = (fileData[i].split(";")); //split the string representation up again, based on the elements of the cell.
+                    int row = Integer.parseInt(elements[0]); //first element is row
+                    int column = Integer.parseInt(elements[1]); //second is col
+                    String input = elements[2]; //third is the actual input of the cell.
+                    ss.addCell(input, row, column);
+                }
+                myFrame = new SSFrame(ss);
             }
         });
     }
@@ -97,7 +115,7 @@ public class FileMenu extends JMenu {
             if (result == JFileChooser.APPROVE_OPTION) {
                 String file = String.valueOf((fileChooser.getSelectedFile()));
                 myFrame.getMySpreadSheet().saveSheet(file);
-                JOptionPane.showMessageDialog(null, "File saved as : " + file);
+                JOptionPane.showMessageDialog(null, "Save Successful");
             }
         });
     }
